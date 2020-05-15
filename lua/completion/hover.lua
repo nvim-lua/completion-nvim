7e65695d1986670448548d3d7dc2c1bb4a864462
haorenW1025 <whz861025@gmail.com>
2020-05-15 09:49:08 +0000

    fix: avoid overwriting custom callback by checking client object(#63)

--- lua/completion/hover.lua
+++ lua/completion/hover.lua
@@ -242,62 +242,67 @@ local fancy_floating_markdown = function(contents, opts)
   return bufnr, winnr
 end
 
--- Modify hover callback
-function M.modifyCallback()
-  local callback = 'textDocument/hover'
-  local default_callback = vim.lsp.callbacks[callback]
-  vim.lsp.callbacks[callback] = function(_, method, result)
-    -- if M.winnr ~= nil and api.nvim_win_is_valid(M.winnr) then
-      -- api.nvim_win_close(M.winnr, true)
-    -- end
-    if vim.fn.pumvisible() == 1 then
-      M.focusable_float(method, function()
-        if not (result and result.contents) then
-          -- return { 'No information available' }
-          return
-        end
-        local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
-        markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
-        if vim.tbl_isempty(markdown_lines) then
-          -- return { 'No information available' }
-          return
-        end
-        local bufnr, winnr
-        -- modified to open hover window align to popupmenu
+local callback = 'textDocument/hover'
+local default_callback = vim.lsp.callbacks[callback]
 
-        local position = vim.fn.pum_getpos()
-        -- Set max width option to avoid overlapping with popup menu
-        local total_column = api.nvim_get_option('columns')
-        local align
-        if position['col'] < total_column/2 then
-          align = 'right'
-        else
-          align = 'left'
-        end
-        bufnr, winnr = fancy_floating_markdown(markdown_lines, {
-          pad_left = 1; pad_right = 1;
-          col = position['col']; width = position['width']; row = position['row']-1;
-          align = align
-        })
-        M.winnr = winnr
+function callback_function(_, method, result)
+  -- if M.winnr ~= nil and api.nvim_win_is_valid(M.winnr) then
+    -- api.nvim_win_close(M.winnr, true)
+  -- end
+  if vim.fn.pumvisible() == 1 then
+    M.focusable_float(method, function()
+      if not (result and result.contents) then
+        -- return { 'No information available' }
+        return
+      end
+      local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+      markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+      if vim.tbl_isempty(markdown_lines) then
+        -- return { 'No information available' }
+        return
+      end
+      local bufnr, winnr
+      -- modified to open hover window align to popupmenu
 
-        vim.lsp.util.close_preview_autocmd({"CursorMoved", "BufHidden", "InsertCharPre"}, winnr)
-        local hover_len = #vim.api.nvim_buf_get_lines(bufnr,0,-1,false)[1]
-        local win_width = vim.api.nvim_win_get_width(0)
-        if hover_len > win_width then
-          vim.api.nvim_win_set_width(winnr,math.min(hover_len,win_width))
-          vim.api.nvim_win_set_height(winnr,math.ceil(hover_len/win_width))
-          vim.wo[winnr].wrap = true
-        end
-        return bufnr, winnr
-      end)
-    else
-      default_callback(_, method, result, _)
-    end
+      local position = vim.fn.pum_getpos()
+      -- Set max width option to avoid overlapping with popup menu
+      local total_column = api.nvim_get_option('columns')
+      local align
+      if position['col'] < total_column/2 then
+        align = 'right'
+      else
+        align = 'left'
+      end
+      bufnr, winnr = fancy_floating_markdown(markdown_lines, {
+        pad_left = 1; pad_right = 1;
+        col = position['col']; width = position['width']; row = position['row']-1;
+        align = align
+      })
+      M.winnr = winnr
+
+      vim.lsp.util.close_preview_autocmd({"CursorMoved", "BufHidden", "InsertCharPre"}, winnr)
+      local hover_len = #vim.api.nvim_buf_get_lines(bufnr,0,-1,false)[1]
+      local win_width = vim.api.nvim_win_get_width(0)
+      if hover_len > win_width then
+        vim.api.nvim_win_set_width(winnr,math.min(hover_len,win_width))
+        vim.api.nvim_win_set_height(winnr,math.ceil(hover_len/win_width))
+        vim.wo[winnr].wrap = true
+      end
+      return bufnr, winnr
+    end)
+  else
+    default_callback(_, method, result, _)
   end
 end
 
 M.autoOpenHoverInPopup = function(manager)
+  for _, client in pairs(vim.lsp.buf_get_clients(0)) do
+    default_callback = client.config.callbacks['textDocument/hover'] or vim.lsp.callbacks['textDocument/hover']
+    if default_callback ~= callback_function then
+      client.config.callbacks['textDocument/hover'] = callback_function
+    end
+  end
+
   local bufnr = api.nvim_get_current_buf()
   if api.nvim_call_function('pumvisible', {}) == 1 then
     -- Auto open hover
