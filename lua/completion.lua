98657eb9b79f5d648cd27cbcfe4049c6df1f3c0a
haorenW1025 <whz861025@gmail.com>
2020-06-26 15:04:03 +0800

    Refactor: move source into subdirectory

--- lua/completion.lua
+++ lua/completion.lua
@@ -1,7 +1,7 @@
 local vim = vim
 local api = vim.api
 local match = require'completion.matching'
-local source = require 'source'
+local source = require 'source.source'
 local signature = require'completion.signature_help'
 local hover = require'completion.hover'
 local M = {}
