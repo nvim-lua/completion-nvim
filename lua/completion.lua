f3707eec765681d8d69265bf4ec6b8a7ef947260
Raphael <glepnir@gopherhub.org>
2020-09-07 17:20:39 +0800

    auto tigger signature help window.

--- lua/completion.lua
+++ lua/completion.lua
@@ -205,6 +205,10 @@ function M.on_CompleteDone()
   if hover.winnr ~= nil and api.nvim_win_is_valid(hover.winnr) then
     api.nvim_win_close(hover.winnr, true)
   end
+  -- auto trigger signature help when we confirm completion
+  if vim.g.completion_enable_auto_signature ~= 0 then
+    vim.lsp.buf.signature_help()
+  end
 end
 
 M.on_attach = function(option)
