5ff2ff695f4b96853821c5bb94a6240271978738
Jorri Fransen <jorri@jorrifransen.nl>
2021-03-23 14:53:08 +0100

    Added syntax highlighting in the signature window.

--- lua/completion/signature_help.lua
+++ lua/completion/signature_help.lua
@@ -42,11 +42,13 @@ M.autoOpenSignatureHelp = function()
       if vim.tbl_isempty(lines) then
         return
       end
+      local syntax = vim.api.nvim_buf_get_option(bufnr, 'syntax')
       local bufnr, _ = vim.lsp.util.focusable_preview(method, function()
         -- TODO show popup when signatures is empty?
         lines = vim.lsp.util.trim_empty_lines(lines)
         return lines, vim.lsp.util.try_trim_markdown_code_blocks(lines)
       end)
+      vim.api.nvim_buf_set_option(bufnr, 'syntax', syntax)
       -- setup a variable for floating window, fix #223
       vim.api.nvim_buf_set_var(bufnr, "lsp_floating", true)
     end)
